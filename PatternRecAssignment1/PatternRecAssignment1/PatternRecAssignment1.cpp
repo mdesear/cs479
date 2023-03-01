0e9937e6aff4ff237405228bb9e465540538b501
ChristopherTrimble <christophertrimble@nevada.unr.edu>
2023-02-28 21:58:46 -0800

    Updated variable names

--- PatternRecAssignment1/PatternRecAssignment1/PatternRecAssignment1.cpp
+++ PatternRecAssignment1/PatternRecAssignment1/PatternRecAssignment1.cpp
@@ -5,15 +5,15 @@
 
 using namespace std;
 
-vector<pair<float, float>>* SetupData(pair<float, float> u1_firstSet, pair<float, float> u1_secondSet, pair<float, float> u2_firstSet, pair<float, float> u2_secondSet)
+vector<pair<float, float>>* SetupData(pair<float, float> u1, pair<float, float> e1, pair<float, float> u2, pair<float, float> e2)
 {
     const auto newData = new vector<pair<float, float>>;
     for(int i = 0; i < 200000; i++)
     {
         if (i < 60000)
-            (*newData).emplace_back(std::make_pair(boxmuller::box_muller(u1_firstSet.first, sqrt(u1_firstSet.second)), boxmuller::box_muller(u1_secondSet.first, sqrt(u1_secondSet.second))));
+            (*newData).emplace_back(std::make_pair(boxmuller::box_muller(u1.first, sqrt(e1.first)), boxmuller::box_muller(u1.second, sqrt(e1.second))));
         else
-            (*newData).emplace_back(std::make_pair(boxmuller::box_muller(u2_firstSet.first, sqrt(u2_firstSet.second)), boxmuller::box_muller(u2_secondSet.first, sqrt(u2_secondSet.second))));
+            (*newData).emplace_back(std::make_pair(boxmuller::box_muller(u2.first, sqrt(e2.first)), boxmuller::box_muller(u2.second, sqrt(e2.second))));
     }
 
     return newData;
@@ -21,7 +21,7 @@ vector<pair<float, float>>* SetupData(pair<float, float> u1_firstSet, pair<float
 
 int main(int argc, char* argv[])
 {
-    vector<pair<float, float>> dataSetA = *SetupData(std::make_pair(1, 1), std::make_pair(1, 1), std::make_pair(4, 1), std::make_pair(4, 1));
+    vector<pair<float, float>> dataSetA = *SetupData(std::make_pair(1, 1), std::make_pair(1, 1), std::make_pair(4, 4), std::make_pair(1, 1));
     vector<pair<float, float>> dataSetB = *SetupData(std::make_pair(1, 1), std::make_pair(1, 1), std::make_pair(4, 4), std::make_pair(4, 8));
 
     for(int i = 0; i < dataSetA.size(); i++)
