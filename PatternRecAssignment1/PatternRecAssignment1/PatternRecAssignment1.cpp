29abb344dc758de61b2580990b735ac08fef666e
ChristopherTrimble <christophertrimble@nevada.unr.edu>
2023-03-01 13:48:23 -0800

    Updated main to use datafiles

--- PatternRecAssignment1/PatternRecAssignment1/PatternRecAssignment1.cpp
+++ PatternRecAssignment1/PatternRecAssignment1/PatternRecAssignment1.cpp
@@ -1,3 +1,4 @@
+#include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -8,10 +9,85 @@ using namespace std;
 using namespace Eigen;
 
 typedef pair<float, float> PairData;
-
 enum Label { ClassOne, ClassTwo };
 
-vector<PairData>* SetupData(PairData u1, Matrix2f e1, PairData u2, Matrix2f e2)
+string DataSetA_File = "Data\\DataSetA.csv";
+string DataSetB_File = "Data\\DataSetB.csv";
+
+//prototypes
+vector<PairData>* SetupData(string fname, PairData, Matrix2f, PairData, Matrix2f);
+vector<PairData>* ReadInData(string);
+void SaveDataToFile(string, vector<PairData>);
+bool CheckIfFileExists(string);
+
+int main(int argc, char* argv[])
+{
+    // Sigma Values
+    Matrix2f matrix1;
+    Matrix2f matrix2;
+    matrix1 << 1, 0, 0, 1;
+    matrix2 << 4, 0, 0, 8;
+    
+    // Mu Values
+    constexpr PairData pair1 = make_pair(1, 1);
+    constexpr PairData pair2 = make_pair(4, 4);
+
+    // Datasets created from the Mu and Sigma values fed into a Boxmuller
+    vector<PairData> dataSetA = CheckIfFileExists(DataSetA_File) ? *ReadInData(DataSetA_File) : *SetupData(DataSetA_File, pair1, matrix1, pair2, matrix1);
+    vector<PairData> dataSetB = CheckIfFileExists(DataSetB_File) ? *ReadInData(DataSetB_File) : *SetupData(DataSetB_File, pair1, matrix1, pair2, matrix2);
+    
+    dataSetA.clear();
+    dataSetB.clear();
+    
+    return 0;
+}
+
+bool CheckIfFileExists(string fname)
+{
+    ifstream file (fname.c_str());
+    return file.good();
+}
+
+vector<PairData>* ReadInData(string fname)
+{
+    const auto newData = new vector<pair<float, float>>;
+    string line, value;
+    
+    fstream file (fname, ios::in);
+    if(file.is_open())
+    {
+        while(getline(file, line))
+        {
+            int i = 0;
+            PairData temp;
+            stringstream str(line);
+            while(getline(str, value, ','))
+            {
+                if (i ==  0)
+                    temp.first = stof(value);
+                else
+                    temp.second = stof(value);
+                i++;
+            }
+            (*newData).emplace_back(temp);
+        }
+    }
+    file.close();
+    return newData;
+}
+
+void SaveDataToFile(string fname, vector<PairData>* dataSet)
+{
+    fstream file (fname, ios::out);
+    if(file.is_open())
+    {
+        for (auto data : *dataSet)
+            file << data.first << "," << data.second << "\n";
+    }
+    file.close();
+}
+
+vector<PairData>* SetupData(string fname, PairData u1, Matrix2f e1, PairData u2, Matrix2f e2)
 {
     const auto newData = new vector<pair<float, float>>;
     for(int i = 0; i < 200000; i++)
@@ -22,30 +98,6 @@ vector<PairData>* SetupData(PairData u1, Matrix2f e1, PairData u2, Matrix2f e2)
             (*newData).emplace_back(make_pair(boxmuller::box_muller(u2.first, sqrt(e2(0, 0))), boxmuller::box_muller(u2.second, sqrt(e2(1, 1)))));
     }
 
+    SaveDataToFile(fname, newData);
     return newData;
-}
-
-int main(int argc, char* argv[])
-{
-    // Sigma Values
-    Matrix2f matrix1;
-    Matrix2f matrix2;
-    matrix1 << 1, 0, 0, 1;
-    matrix2 << 4, 0, 0, 8;
-
-    // Mu Values
-    constexpr PairData pair1 = make_pair(1, 1);
-    constexpr PairData pair2 = make_pair(4, 4);
-
-    // Datasets created from the Mu and Sigma values fed into a Boxmuller
-    vector<PairData> dataSetA = *SetupData(pair1, matrix1, pair2, matrix1);
-    vector<PairData> dataSetB = *SetupData(pair1, matrix1, pair2, matrix2);
-    
-    for(int i = 0; i < dataSetA.size(); i++)
-        printf("Data Set 1: %.4f %.4f\tData Set 2: %.4f %.4f\n", dataSetA[i].first, dataSetA[i].second, dataSetB[i].first, dataSetB[i].second);
-    
-    dataSetA.clear();
-    dataSetB.clear();
-    
-    return 0;
 }
