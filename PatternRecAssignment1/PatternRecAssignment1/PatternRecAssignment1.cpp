98411693c25b34cc973fd000ce547f972a49d709
ChristopherTrimble <christophertrimble@nevada.unr.edu>
2023-02-28 21:48:42 -0800

    Changes to main

--- PatternRecAssignment1/PatternRecAssignment1/PatternRecAssignment1.cpp
+++ PatternRecAssignment1/PatternRecAssignment1/PatternRecAssignment1.cpp
@@ -5,15 +5,15 @@
 
 using namespace std;
 
-vector<pair<float, float>>* SetupData(float m1, float s1, float m2, float s2)
+vector<pair<float, float>>* SetupData(pair<float, float> u1_firstSet, pair<float, float> u1_secondSet, pair<float, float> u2_firstSet, pair<float, float> u2_secondSet)
 {
     const auto newData = new vector<pair<float, float>>;
     for(int i = 0; i < 200000; i++)
     {
         if (i < 60000)
-            (*newData).emplace_back(std::make_pair(boxmuller::box_muller(m1, s1), boxmuller::box_muller(m1, s1)));
+            (*newData).emplace_back(std::make_pair(boxmuller::box_muller(u1_firstSet.first, sqrt(u1_firstSet.second)), boxmuller::box_muller(u1_secondSet.first, sqrt(u1_secondSet.second))));
         else
-            (*newData).emplace_back(std::make_pair(boxmuller::box_muller(m2, s2), boxmuller::box_muller(m2, s2)));
+            (*newData).emplace_back(std::make_pair(boxmuller::box_muller(u2_firstSet.first, sqrt(u2_firstSet.second)), boxmuller::box_muller(u2_secondSet.first, sqrt(u2_secondSet.second))));
     }
 
     return newData;
@@ -21,8 +21,8 @@ vector<pair<float, float>>* SetupData(float m1, float s1, float m2, float s2)
 
 int main(int argc, char* argv[])
 {
-    vector<pair<float, float>> dataSetA = *SetupData(1, 1, 1, 1);
-    vector<pair<float, float>> dataSetB = *SetupData(1, 1, 4, 8);
+    vector<pair<float, float>> dataSetA = *SetupData(std::make_pair(1, 1), std::make_pair(1, 1), std::make_pair(4, 1), std::make_pair(4, 1));
+    vector<pair<float, float>> dataSetB = *SetupData(std::make_pair(1, 1), std::make_pair(1, 1), std::make_pair(4, 4), std::make_pair(4, 8));
 
     for(int i = 0; i < dataSetA.size(); i++)
         printf("Data Set 1: %.4f %.4f\tData Set 2: %.4f %.4f\t Number: %i\n", dataSetA[i].first, dataSetA[i].second, dataSetB[i].first, dataSetB[i].second, i + 1);
