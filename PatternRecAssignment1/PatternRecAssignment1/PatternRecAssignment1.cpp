1c70ac1d541f56c6616aae9af4e4e90d3100adcf
ChristopherTrimble <christophertrimble@nevada.unr.edu>
2023-03-01 00:03:53 -0800

    Updated variables

--- PatternRecAssignment1/PatternRecAssignment1/PatternRecAssignment1.cpp
+++ PatternRecAssignment1/PatternRecAssignment1/PatternRecAssignment1.cpp
@@ -11,15 +11,15 @@ typedef pair<float, float> PairData;
 
 enum Label { ClassOne, ClassTwo };
 
-vector<PairData>* SetupData(PairData u1, PairData e1, PairData u2, PairData e2)
+vector<PairData>* SetupData(PairData u1, Matrix2f e1, PairData u2, Matrix2f e2)
 {
     const auto newData = new vector<pair<float, float>>;
     for(int i = 0; i < 200000; i++)
     {
         if (i < 60000)
-            (*newData).emplace_back(make_pair(boxmuller::box_muller(u1.first, sqrt(e1.first)), boxmuller::box_muller(u1.second, sqrt(e1.second))));
+            (*newData).emplace_back(make_pair(boxmuller::box_muller(u1.first, sqrt(e1(0, 0))), boxmuller::box_muller(u1.second, sqrt(e1(1, 1)))));
         else
-            (*newData).emplace_back(make_pair(boxmuller::box_muller(u2.first, sqrt(e2.first)), boxmuller::box_muller(u2.second, sqrt(e2.second))));
+            (*newData).emplace_back(make_pair(boxmuller::box_muller(u2.first, sqrt(e2(0, 0))), boxmuller::box_muller(u2.second, sqrt(e2(1, 1)))));
     }
 
     return newData;
@@ -27,19 +27,22 @@ vector<PairData>* SetupData(PairData u1, PairData e1, PairData u2, PairData e2)
 
 int main(int argc, char* argv[])
 {
-    vector<PairData> dataSetA = *SetupData(make_pair(1, 1), make_pair(1, 1), make_pair(4, 4), make_pair(1, 1));
-    vector<PairData> dataSetB = *SetupData(make_pair(1, 1), make_pair(1, 1), make_pair(4, 4), make_pair(4, 8));
-    
-    Matrix2f matrix_A1;
-    Matrix2f matrix_B2;
+    // Sigma Values
+    Matrix2f matrix1;
+    Matrix2f matrix2;
+    matrix1 << 1, 0, 0, 1;
+    matrix2 << 4, 0, 0, 8;
 
-    matrix_A1 << 1, 0, 0, 1;
-    Matrix2f matrix_A2 = matrix_A1;
-    Matrix2f matrix_B1 = matrix_A1;
-    matrix_B2 << 4, 0, 0, 8;
+    // Mu Values
+    constexpr PairData pair1 = make_pair(1, 1);
+    constexpr PairData pair2 = make_pair(4, 4);
+
+    // Datasets created from the Mu and Sigma values fed into a Boxmuller
+    vector<PairData> dataSetA = *SetupData(pair1, matrix1, pair2, matrix1);
+    vector<PairData> dataSetB = *SetupData(pair1, matrix1, pair2, matrix2);
     
     for(int i = 0; i < dataSetA.size(); i++)
-        printf("Data Set 1: %.4f %.4f\tData Set 2: %.4f %.4f\t Number: %i\n", dataSetA[i].first, dataSetA[i].second, dataSetB[i].first, dataSetB[i].second, i + 1);
+        printf("Data Set 1: %.4f %.4f\tData Set 2: %.4f %.4f\n", dataSetA[i].first, dataSetA[i].second, dataSetB[i].first, dataSetB[i].second);
     
     dataSetA.clear();
     dataSetB.clear();
