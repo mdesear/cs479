9a519f21247f68090b60700df13a74b46a03ecb3
ChristopherTrimble <christophertrimble@nevada.unr.edu>
2023-02-28 21:33:37 -0800

    Updated Main

--- PatternRecAssignment1/PatternRecAssignment1/PatternRecAssignment1.cpp
+++ PatternRecAssignment1/PatternRecAssignment1/PatternRecAssignment1.cpp
@@ -5,12 +5,15 @@
 
 using namespace std;
 
-vector<pair<float, float>>* SetupData(float m, float s)
+vector<pair<float, float>>* SetupData(float m1, float s1, float m2, float s2)
 {
     const auto newData = new vector<pair<float, float>>;
-    for(int i = 0; i < 200001; i++)
+    for(int i = 0; i < 200000; i++)
     {
-        (*newData).emplace_back(std::make_pair(boxmuller::box_muller(m, s), boxmuller::box_muller(m, s)));
+        if (i < 60000)
+            (*newData).emplace_back(std::make_pair(boxmuller::box_muller(m1, s1), boxmuller::box_muller(m1, s1)));
+        else
+            (*newData).emplace_back(std::make_pair(boxmuller::box_muller(m2, s2), boxmuller::box_muller(m2, s2)));
     }
 
     return newData;
@@ -18,8 +21,8 @@ vector<pair<float, float>>* SetupData(float m, float s)
 
 int main(int argc, char* argv[])
 {
-    vector<pair<float, float>> dataSetA = *SetupData(1, 1);
-    vector<pair<float, float>> dataSetB = *SetupData(4, 4);
+    vector<pair<float, float>> dataSetA = *SetupData(1, 1, 1, 1);
+    vector<pair<float, float>> dataSetB = *SetupData(1, 1, 4, 8);
 
     for(int i = 0; i < dataSetA.size(); i++)
         printf("Data Set 1: %.4f %.4f\tData Set 2: %.4f %.4f\t Number: %i\n", dataSetA[i].first, dataSetA[i].second, dataSetB[i].first, dataSetB[i].second, i + 1);
