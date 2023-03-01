9f1f1ae210b4e1f76414e2f51c3c26cacb2f4f79
ChristopherTrimble <christophertrimble@nevada.unr.edu>
2023-02-28 23:31:21 -0800

    New Eigen stuff

--- PatternRecAssignment1/PatternRecAssignment1/PatternRecAssignment1.cpp
+++ PatternRecAssignment1/PatternRecAssignment1/PatternRecAssignment1.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <Eigen/Dense>
 #include "boxmuller.h"
 
 using namespace std;
+using namespace Eigen;
 
-vector<pair<float, float>>* SetupData(pair<float, float> u1, pair<float, float> e1, pair<float, float> u2, pair<float, float> e2)
+typedef pair<float, float> PairData;
+
+enum Label { ClassOne, ClassTwo };
+
+vector<PairData>* SetupData(PairData u1, PairData e1, PairData u2, PairData e2)
 {
     const auto newData = new vector<pair<float, float>>;
     for(int i = 0; i < 200000; i++)
     {
         if (i < 60000)
-            (*newData).emplace_back(std::make_pair(boxmuller::box_muller(u1.first, sqrt(e1.first)), boxmuller::box_muller(u1.second, sqrt(e1.second))));
+            (*newData).emplace_back(make_pair(boxmuller::box_muller(u1.first, sqrt(e1.first)), boxmuller::box_muller(u1.second, sqrt(e1.second))));
         else
-            (*newData).emplace_back(std::make_pair(boxmuller::box_muller(u2.first, sqrt(e2.first)), boxmuller::box_muller(u2.second, sqrt(e2.second))));
+            (*newData).emplace_back(make_pair(boxmuller::box_muller(u2.first, sqrt(e2.first)), boxmuller::box_muller(u2.second, sqrt(e2.second))));
     }
 
     return newData;
@@ -21,9 +27,17 @@ vector<pair<float, float>>* SetupData(pair<float, float> u1, pair<float, float> 
 
 int main(int argc, char* argv[])
 {
-    vector<pair<float, float>> dataSetA = *SetupData(std::make_pair(1, 1), std::make_pair(1, 1), std::make_pair(4, 4), std::make_pair(1, 1));
-    vector<pair<float, float>> dataSetB = *SetupData(std::make_pair(1, 1), std::make_pair(1, 1), std::make_pair(4, 4), std::make_pair(4, 8));
+    vector<PairData> dataSetA = *SetupData(make_pair(1, 1), make_pair(1, 1), make_pair(4, 4), make_pair(1, 1));
+    vector<PairData> dataSetB = *SetupData(make_pair(1, 1), make_pair(1, 1), make_pair(4, 4), make_pair(4, 8));
+    
+    Matrix2f matrix_A1;
+    Matrix2f matrix_B2;
 
+    matrix_A1 << 1, 0, 0, 1;
+    Matrix2f matrix_A2 = matrix_A1;
+    Matrix2f matrix_B1 = matrix_A1;
+    matrix_B2 << 4, 0, 0, 8;
+    
     for(int i = 0; i < dataSetA.size(); i++)
         printf("Data Set 1: %.4f %.4f\tData Set 2: %.4f %.4f\t Number: %i\n", dataSetA[i].first, dataSetA[i].second, dataSetB[i].first, dataSetB[i].second, i + 1);
     
